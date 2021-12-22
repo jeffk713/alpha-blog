deab9581a254080c9243dd0e68d31d2246d37801
jeff <jeffk713@gmail.com>
2021-12-21 19:31:06 -0800

    restore all the features so far

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,22 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id]) # instance variable is needed
+  end
+
+  def new
+    @article = Article.new # for initial render of the page
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description)) # Rails will take title and description from the article 
+    if @article.save
+      redirect_to @article # short-cut for 'redirect_to article_path(@article)'
+    else
+      render 'new'
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  
+  resources :articles
+  
+  # to access all articles requests
+  resources :articles, only: [:index, :show, :new, :create] # specify to access only certain actions
 end
