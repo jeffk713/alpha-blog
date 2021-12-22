5e04e1b38968d33b581705c2097055b4ecb52543
jeff <jeffk713@gmail.com>
2021-12-21 22:10:02 -0800

    implement update action

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :articles
   
   # to access all articles requests
-  resources :articles, only: [:index, :show, :new, :create] # specify to access only certain actions
+  resources :articles, only: [:index, :show, :new, :create, :edit, :update] # specify to access only certain actions
 end
