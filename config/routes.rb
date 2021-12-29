0f5453ff556abdd66505e072340638d9783096b7
jeff <jeffk713@gmail.com>
2021-12-28 23:11:24 -0800

    add user create action and add signup link in homepage

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
     
   # to access all articles requests
   resources :articles #, only: [:index, :show, :new, :create, :edit, :update. :destroy] # specify to access only certain actions
+  
+  get 'signup', to: 'users#new'
+
+  resources :users, except: [:new] # since `new` action is used by GET request 'signup'
 end
